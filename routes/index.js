d0f89bdbb7602e702d1c7c92394bbc5abc5a7bea
Raj3124 <rajmj3113@gmail.com>
2020-11-21 17:33:41 -0500

    created required files

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+// const gameroutes = require('./')  import routes here
+
+const constructorMethod = (app) =>{
+    // app.use()
+
+    // app.use('*', (req,res) => {
+    //     res.status(404).render()
+    // });
+};
+
+module.exports = constructorMethod;
