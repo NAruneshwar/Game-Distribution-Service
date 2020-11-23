ba63032a7e3280b86d0dc10018b7a2f79e69b55e
Raj3124 <rajmj3113@gmail.com>
2020-11-22 23:25:51 -0500

    updated

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express()
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const configRoutes = require('./routes')
 
+const static = express.static(__dirname + '/public');
+const exphbs = require("express-handlebars");
+app.use("/public", static);
+app.use(cookieParser())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.engine('handlebars', exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(
+    session({
+        name: 'gamedistribution',
+        secret: 'cookieforgames',
+        resave: false,
+        saveUninitialized: true
+    }));
+
 configRoutes(app)
 
 app.listen(3000, () => {
