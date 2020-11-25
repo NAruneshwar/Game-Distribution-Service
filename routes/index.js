2582c9ef4c8620bdf4a288ae8c4ba989be59684e
Raj3124 <rajmj3113@gmail.com>
2020-11-24 20:04:59 -0500

    Added new routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
-// const gameroutes = require('./')  import routes here
+const gamesroutes = require('./games')
+const commentroutes = require('./comments')
+const reviewroutes = require('./reviews')
 
-const constructorMethod = (app) =>{
-    // app.use()
+const constructorMethod = (app) => {
+    // app.use('/', ())  ----> USE THIS FOR LOADING DASHBOARD
+    // app.use('games/', gamesroutes);
+    // app.use('comments/', commentroutes);
+    // app.use('reviews/', reviewroutes);
 
-    // app.use('*', (req,res) => {
-    //     res.status(404).render()
-    // });
+    app.use('*', (req, res) => {
+        res.status(404).render("posts/errors", { title: "Error", statusCode: "404", message: "The page that you are looking for does not exist!" })
+    });
 };
 
 module.exports = constructorMethod;
