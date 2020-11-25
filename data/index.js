ec5d2a64dec8a2b60e26d06226069b5b83ef31fd
Raj3124 <rajmj3113@gmail.com>
2020-11-24 20:05:40 -0500

    New Files

---
+++ data/index.js
@@ -0,0 +1,7 @@
+const gamesData = require('./games');
+const reviewsData = require('./reviews');
+
+module.exports = { // Add other data files if required
+    games: gamesData,
+    reviews: reviewsData
+};
