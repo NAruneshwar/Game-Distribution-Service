2cd8f431ddaf408c2639b62565dc961048469664
Raj3124 <rajmj3113@gmail.com>
2020-12-15 07:13:33 -0500

    Login route completed except sessions

--- routes/login.js
+++ routes/login.js
@@ -11,9 +11,9 @@ router.get('/', async (req, res) => {
 router.post('/check', async(req,res)=>{
     username=req.body.username
     password = req.body.password
-
+    flag=0
     const users = await userdata.allUsers();
-
+    //session handling remaining
     for(i=0;i<users.length;i++){
         if(users[i].username==username){
             try{
@@ -25,7 +25,13 @@ router.post('/check', async(req,res)=>{
             if(compared==true){
                 // activate session here
                 res.status(200).res.render("posts/homepage",{title: "Home page"});
+                return;
             }
+        }
+        flag+=1
+        if(flag==users.length){
+            res.status(401).render("posts/login",{title: "Login", message:"Username or password is not valid!"})
+                return;
         }
     }
 });
