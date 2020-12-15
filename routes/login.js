4d3259746b184eeaf780c41b77985cde64a6d90d
Raj3124 <rajmj3113@gmail.com>
2020-12-15 02:56:27 -0500

    login functionality done- requires data function

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,33 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const userdata = require('../data/users')
+
 
 router.get('/', async (req, res) => { 
-    res.render("posts/login",{title: "Log In"});
-  
+    res.render("posts/login",{title: "Log In"});  
+});
+
+router.post('/check', async(req,res)=>{
+    username=req.body.username
+    password = req.body.password
+
+    const users = await userdata.allUsers();
+
+    for(i=0;i<users.length;i++){
+        if(users[i].username==username){
+            try{
+                compared = await bcrypt.compare(password, users[i].hashedPassword)
+            }catch(e){
+                res.status(401).render("posts/login",{title: "Login", message:"Username or password is not valid!"})
+                return;
+            }
+            if(compared==true){
+                // activate session here
+                res.status(200).res.render("posts/homepage",{title: "Home page"});
+            }
+        }
+    }
 });
 
 module.exports = router;
