25dadb2433a5ea1bac805fd6ef8375e8dd866c94
Raj3124 <rajmj3113@gmail.com>
2020-12-16 01:01:40 -0500

    debugged

--- routes/login.js
+++ routes/login.js
@@ -1,38 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const userdata = require('../data/users')
+const logindata = require('../data/login')
 
+function checkUsernamePassword(uname, pswd) {
+    uname = uname.trim()
+    if (uname == "" || typeof uname != 'string' || !uname){
+        throw `Please enter username`
+    }
 
-router.get('/', async (req, res) => { 
-    res.render("posts/login",{title: "Log In"});  
+    if (pswd == "" || typeof pswd != 'string' || !pswd){
+        throw `Please enter password`
+    }
+}
+
+router.get('/', async (req, res) => {
+    res.render("posts/login", { title: "Log In" });
 });
 
-router.post('/check', async(req,res)=>{
-    username=req.body.username
+router.post('/check', async (req, res) => {
+    username = req.body.username
     password = req.body.password
-    flag=0
-    const users = await userdata.allUsers();
-    //session handling remaining
-    for(i=0;i<users.length;i++){
-        if(users[i].username==username){
-            try{
-                compared = await bcrypt.compare(password, users[i].hashedPassword)
-            }catch(e){
-                res.status(401).render("posts/login",{title: "Login", message:"Username or password is not valid!"})
-                return;
-            }
-            if(compared==true){
-                // activate session here
-                res.status(200).res.render("posts/homepage",{title: "Home page"});
-                return;
-            }
-        }
-        flag+=1
-        if(flag==users.length){
-            res.status(401).render("posts/login",{title: "Login", message:"Username or password is not valid!"})
-                return;
-        }
+    checkUsernamePassword(username, password)
+    hashedPassword = await bcrypt.hash(password, 16)
+    try{
+    const users = await logindata.check(username, hashedPassword);
+    }
+    catch(e){
+        res.status(401).render('/',{message: e})
     }
 });
 
