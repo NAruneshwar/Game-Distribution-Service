c5678e3e3c7ffbc5baadfaa005a6a07c00a7d5f5
Raj3124 <rajmj3113@gmail.com>
2020-12-16 06:21:45 -0500

    New file for profile data

---
+++ data/profile.js
@@ -0,0 +1,17 @@
+// should contain the following functions
+
+const deleteProfileById = async(user_id)=>{
+
+}
+
+const getUserInfo =async(user_id)=>{
+
+}
+
+module.exports={
+    deleteProfileById,
+    getUserInfo
+}
+
+
+
