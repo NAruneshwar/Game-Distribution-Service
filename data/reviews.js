249537da0de34598b4cf96f958cc3bc1c70350f1
NAruneshwar <57510031+NAruneshwar@users.noreply.github.com>
2020-12-16 17:06:07 +0530

    i dont understand how hit works with conficts

--- data/reviews.js
+++ data/reviews.js
@@ -1,6 +1,7 @@
 // import all required files
 const mongoCollections = require('../config/mongoCollections.js');
 let { ObjectId } = require('mongodb');
+const reviews = mongoCollections.reviews;
 
 
 const reviewsByGameId = async(game_id)=>{
