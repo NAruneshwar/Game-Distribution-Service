49ed3504bf031dfaa20faacfcdf3d732d834e6cc
Raj Mahajan <rajmj3113@gmail.com>
2020-12-16 06:46:34 -0500

    Update payment.js

--- data/payment.js
+++ data/payment.js
@@ -1,7 +1,7 @@
 // for data related to payment
 
 const getPriceForGame = async (game_id) => {
-    
+    //Get all details of this game which you think are necessary to display on payments page
 }
 
 const addToGamersProfile = async(game_id)=>{
