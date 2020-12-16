936f164d658ae5cb0d229af56056cc9c2dadfd31
Raj3124 <rajmj3113@gmail.com>
2020-12-16 01:40:11 -0500

    TODO

--- data/login.js
+++ data/login.js
@@ -2,3 +2,5 @@
 
 
 // create a function called check(username,hashedPassword) to check username and password of user
+
+// create a function called admin_check(username,hashedPassword) to check username and password of admin
