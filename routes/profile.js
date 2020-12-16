095266292ff5f4c3f3789ad53b0f3e9f4c6ba9f3
Raj3124 <rajmj3113@gmail.com>
2020-12-16 06:18:17 -0500

    completed routes for profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const profileData  = require('../data/profile');
 
-router.get('/', async (req, res) => {
-    // res.render("posts/login", { title: "Log In" });
-    console.log("in profile");
+router.get('/:user_id', async (req, res) => {
+    let user_id=req.params.user_id
+    try{
+        let user = await profileData.getUserInfo(user_id);
+        res.render("posts/profile",{title: "Your profile", data: user})
+    }catch(e){
+        res.status(401).render("posts/profile",{title: "Your profile", message: e})
+    }
+});
+
+router.delete('/delete/:user_id',async(req,res)=>{ //for admin only... sessions required
+    //Delete a player's profile.
+    try {
+        let user_id = req.params.user_id
+        const deletedProfile = await booksData.deleteProfileById(user_id)
+        if (deletedProfile == 1) {
+            res.render("posts/admin-homepage", { title: "Admin Homepage", message: "Profile Deleted" });
+        }
+    } catch (e) {
+        console.log(e)
+        res.status(500).send({ message: e });
+    }
+
 });
 
 module.exports = router;
