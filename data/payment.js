7f85618fa7c5cd4def8485ba61f0705f02d08e4e
Raj3124 <rajmj3113@gmail.com>
2020-12-16 06:12:49 -0500

    Included required functions to complete

--- data/payment.js
+++ data/payment.js
@@ -1,1 +1,14 @@
 // for data related to payment
+
+const getPriceForGame = async (game_id) => {
+    
+}
+
+const addToGamersProfile = async(game_id)=>{
+    // add the provided game_id to gamer's profile
+}
+
+module.exports = {
+    getPriceForGame,
+    addToGamersProfile
+}
---
+++ data/reviews.js
@@ -0,0 +1,21 @@
+// import all required files
+const mongoCollections = require('../config/mongoCollections.js');
+let { ObjectId } = require('mongodb');
+
+
+const reviewsByGameId = async(game_id)=>{
+
+}
+const deleteGameById= async(review_id)=>{
+
+}
+
+const addReviewForGame= async(game_id)=>{
+
+}
+
+module.exports={
+    reviewsByGameId,
+    deleteGameById,
+    addReviewForGame
+}
