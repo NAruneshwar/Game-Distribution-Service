cf71d41fa89340ac5dd97445abc40654722b6119
Raj3124 <rajmj3113@gmail.com>
2020-12-16 06:17:41 -0500

    New routes

--- routes/payment.js
+++ routes/payment.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    // res.render("posts/login", { title: "Log In" });
-    console.log("in payments");
+router.get('/:game_id', async (req, res) => {
+    //Display payment details of the game
+    //getPriceForGame
+});
+
+router.post('/success',async(req,res)=>{
+    //Add the game to gamer's profile and display a static page with success message with a link to home page
+    //addToGamersProfile
 });
 
 module.exports = router;
