7a1bac23b80e69ccd2585d32a13d42c6f1a31190
Raj3124 <rajmj3113@gmail.com>
2020-12-17 00:55:35 -0500

    Removed unnecessary code and corrected ID check

--- data/games.js
+++ data/games.js
@@ -1,261 +1,145 @@
 const mongoCollections = require('../config/mongoCollections.js');
-let { ObjectId } = require('mongodb');
+let  objectId  = require('mongodb');
 const games = mongoCollections.games;
 
-const create = async (name,image,genre,size,compatibility,languages,age_rating,website,rating,reviews) =>{
-    const gamesCollect = await games();
-    
-    if(!name|| typeof(name)!='string') {
-        throw 'You must provide a name for the book in string format';
-    }
-    if(name.trim()=== ""){
-        throw 'the given name is empty string please provide the name of the game';
-    }
-    //  Processing images only send in the image name.
-      if (!genre || !Array.isArray(genre)){
-        throw 'You must provide an array of genre';
-      }
-      if (genre.length === 0) 
-        throw 'You must provide at least one genre.';
-
-    if(!genre || !Array.isArray(genre)){
-        throw 'You must provide an array of genre';
-      }
-    if(genre.length === 0)
-        throw 'You must provide at least one genre.';
-
-    if(!size|| typeof(size)!='string') {
-        throw 'You must provide a size for the game in string format';
-    }
-    if(size.trim()=== ""){
-        throw 'the given size is empty string please provide a size'
-    }
-
-    if(!compatibility || !Array.isArray(compatibility)){
-        throw 'You must provide an array of compatible devices';
-    }
-    if(compatibility.length === 0) 
-        throw 'You must provide at least one compatible device.';
-    
-
-    if(!languages || !Array.isArray(languages)){
-        throw 'You must provide an array of languages that game supports';
-    }
-    if(languages.length === 0) 
-        throw 'You must provide at least one language that game supports.';
-
-
-    if(!genre || !Array.isArray(genre)){
-        throw 'You must provide a genre or an array of genres';
-      }
-    if(genre.length === 0) 
-        throw 'You must provide a genre or an array of genres.';
-
-
-    if(!age_rating|| typeof(age_rating)!='string') {
-        throw 'You must provide the age_rating for the game in string format';
-      }
-    if(age_rating.trim()=== ""){
-        throw 'the given age_rating is empty string please provide a value in age_rating'
-    }
-
-    if(!website|| typeof(website)!='string') {
-        throw 'You must provide a website for the game in a string format';
-    }
-    if(website.trim()=== ""){
-        throw 'the given website is empty string please provide a website link'
-    }
-    // actually we dont add reviews here.
-    if(!reviews || !Array.isArray(reviews)){
-        throw 'You must provide an array of reviews';
-    }
-    if(reviews.length === 0) 
-        throw 'You must provide at least one reviews.';
-      
-    
-    
-    let newGame ={
-        name,
-        image,
-        genre,
-        size,
-        compatibility,
-        languages,
-        age_rating,
-        website,
-        rating,
-        reviews,
-        "no_of_downloads" : 0
-    };
-    const insertInfo = await gamesCollect.insertOne(newGame);
-    if (insertInfo.insertedCount === 0) throw 'Could not add Game please debug';
-    const newId = insertInfo.insertedId;
-
-    return newId
+const check_id = async (gameid) => {
+  if (!gameid || typeof gameid !== 'string') {
+    throw `ID is not proper`
+  }
+  gameid = userid.trim()
+  if (gameid == "") {
+    throw `ID should not be blank`
+  }
+  if (objectId.isValid(gameid) === false) {
+    throw `ID is not valid`
+  }
 }
 
-
-const update = async (id, name,image,genre,size,compatibility,languages,age_rating,website,rating,reviews, no_of_downloads) =>{
+const create = async (name, image, genre, size, compatibility, languages, age_rating, website, rating, reviews) => {
   const gamesCollect = await games();
-  if (!id){
-    throw 'You must provide an id to search for';
+
+  if (!name || typeof (name) != 'string') {
+    throw 'You must provide a name for the book in string format';
   }
-  var re =  /^[0-9a-fA-F]+$/;
-  if(!re.test(id)) {
-    throw 'Given Input is not in hexadecimal please verify ID'
-  } 
-  if(!name|| typeof(name)!='string') {
-      throw 'You must provide a name for the book in string format';
-  }
-  if(name.trim()=== ""){
-      throw 'the given name is empty string please provide the name of the game';
+  if (name.trim() === "") {
+    throw 'the given name is empty string please provide the name of the game';
   }
   //  Processing images only send in the image name.
-    if (!genre || !Array.isArray(genre)){
-      throw 'You must provide an array of genre';
-    }
-    if (genre.length === 0) 
-      throw 'You must provide at least one genre.';
-
-  if(!genre || !Array.isArray(genre)){
-      throw 'You must provide an array of genre';
-    }
-  if(genre.length === 0)
-      throw 'You must provide at least one genre.';
-
-  if(!size|| typeof(size)!='string') {
-      throw 'You must provide a size for the game in string format';
+  if (!genre || !Array.isArray(genre)) {
+    throw 'You must provide an array of genre';
   }
-  if(size.trim()=== ""){
-      throw 'the given size is empty string please provide a size'
+  if (genre.length === 0)
+    throw 'You must provide at least one genre.';
+
+  if (!genre || !Array.isArray(genre)) {
+    throw 'You must provide an array of genre';
   }
+  if (genre.length === 0)
+    throw 'You must provide at least one genre.';
 
-  if(!compatibility || !Array.isArray(compatibility)){
-      throw 'You must provide an array of compatible devices';
+  if (!size || typeof (size) != 'string') {
+    throw 'You must provide a size for the game in string format';
   }
-  if(compatibility.length === 0) 
-      throw 'You must provide at least one compatible device.';
-  
-
-  if(!languages || !Array.isArray(languages)){
-      throw 'You must provide an array of languages that game supports';
-  }
-  if(languages.length === 0) 
-      throw 'You must provide at least one language that game supports.';
-
-
-  if(!genre || !Array.isArray(genre)){
-      throw 'You must provide a genre or an array of genres';
-    }
-  if(genre.length === 0) 
-      throw 'You must provide a genre or an array of genres.';
-
-
-  if(!age_rating|| typeof(age_rating)!='string') {
-      throw 'You must provide the age_rating for the game in string format';
-    }
-  if(age_rating.trim()=== ""){
-      throw 'the given age_rating is empty string please provide a value in age_rating'
+  if (size.trim() === "") {
+    throw 'the given size is empty string please provide a size'
   }
 
-  if(!website|| typeof(website)!='string') {
-      throw 'You must provide a website for the game in a string format';
+  if (!compatibility || !Array.isArray(compatibility)) {
+    throw 'You must provide an array of compatible devices';
   }
-  if(website.trim()=== ""){
-      throw 'the given website is empty string please provide a website link'
+  if (compatibility.length === 0)
+    throw 'You must provide at least one compatible device.';
+
+
+  if (!languages || !Array.isArray(languages)) {
+    throw 'You must provide an array of languages that game supports';
+  }
+  if (languages.length === 0)
+    throw 'You must provide at least one language that game supports.';
+
+
+  if (!genre || !Array.isArray(genre)) {
+    throw 'You must provide a genre or an array of genres';
+  }
+  if (genre.length === 0)
+    throw 'You must provide a genre or an array of genres.';
+
+
+  if (!age_rating || typeof (age_rating) != 'string') {
+    throw 'You must provide the age_rating for the game in string format';
+  }
+  if (age_rating.trim() === "") {
+    throw 'the given age_rating is empty string please provide a value in age_rating'
+  }
+
+  if (!website || typeof (website) != 'string') {
+    throw 'You must provide a website for the game in a string format';
+  }
+  if (website.trim() === "") {
+    throw 'the given website is empty string please provide a website link'
   }
   // actually we dont add reviews here.
-  if(!reviews || !Array.isArray(reviews)){
-      throw 'You must provide an array of reviews';
+  if (!reviews || !Array.isArray(reviews)) {
+    throw 'You must provide an array of reviews';
   }
-  if(reviews.length === 0) 
-      throw 'You must provide at least one reviews.';
-    
-  
-  
-  let updatedGame ={
-      name,
-      image,
-      genre,
-      size,
-      compatibility,
-      languages,
-      age_rating,
-      website,
-      rating,
-      reviews,
-      no_of_downloads
-  };
-  const insertInfo = await gamesCollect.updateOne({_id: id},{$set: updatedGame});
-  if (insertInfo.insertedCount === 0) throw 'Could not add Game please debug';
+  if (reviews.length === 0)
+    throw 'You must provide at least one reviews.';
 
-  return "Success"
+
+
+  let newGame = {
+    name,
+    image,
+    genre,
+    size,
+    compatibility,
+    languages,
+    age_rating,
+    website,
+    rating,
+    reviews,
+    "no_of_downloads": 0
+  };
+  const insertInfo = await gamesCollect.insertOne(newGame);
+  if (insertInfo.insertedCount === 0) throw 'Could not add Game please debug';
+  const newId = insertInfo.insertedId;
+
+  return newId
 }
 
-const getAll = async() =>{
-    const gamesCollect = await games();
-    const gamesList = await gamesCollect.find({}).toArray();
-    if(gamesList == null) throw 'No games exist in the DB';
-    for(i=0;i<gamesList.length;i++){
-        gamesList[i]._id = gamesList[i]._id.toString();
-    }
-    return gamesList
+const getAll = async () => {
+  const gamesCollect = await games();
+  const gamesList = await gamesCollect.find({}).toArray();
+  if (gamesList == null) throw 'No games exist in the DB';
+  for (i = 0; i < gamesList.length; i++) {
+    gamesList[i]._id = gamesList[i]._id.toString();
   }
+  return gamesList
+}
 
 
-  const getOne = async(game_id) =>{
-    const gamesCollect = await games();
-    const gamesList = await gamesCollect.findOne({"_id": ObjectId(game_id)});;
-    if(gamesList == null) throw 'No game exist in the DB with that id';
-    gamesList._id = gamesList._id.toString();
+const getOne = async (game_id) => {
+  const gamesCollect = await games();
+  const gamesList = await gamesCollect.findOne({ "_id": ObjectId(game_id) });;
+  if (gamesList == null) throw 'No game exist in the DB with that id';
+  gamesList._id = gamesList._id.toString();
+  return gamesList
+}
 
-    // let result =[]
-    // // console.log(Info.length)
-    // for (let k=0; k<Info.length; k++){
-    //   let inner_val = {}
-    //   for (const [key, value] of Object.entries(Info[k])){
-    //     if(key == '_id'){
-    //       inner_val['_id'] = value.toString();
-    //     }
-    //     if(key == 'title'){
-    //       inner_val['title'] = value;
-    //     }
-    //   }
-    //   // console.log(inner_val);
-    //   result.push(inner_val);
-    // }
-    return gamesList
+const remove = async (id, name) => {
+  check_id(id)
+  gamesCollect = await games();
+  const deletionGame = await gamesCollect.deleteOne({ _id: id });
+  if (deletionGame.deletedCount === 0) {
+    throw `Could not delete game.`;
   }
-
-const remove = async(id, name) => {
-    if (!id) {
-      throw 'You must provide an id to delete game';
-    }
-   
-    var re =  /^[0-9a-fA-F]+$/;
-    if(!re.test(id)) {
-      throw 'Given Input is not in hexadecimal please verify ID'
-    } 
-    // gametitle = await get(id)
-    // let reviewcount = booktitle.reviews.length
-    // console.log(movietitle)
-    gamesCollect = await games();
-    const deletionGame = await gamesCollect.deleteOne({ _id: id });
-      // console.log(deletionMovie);
-      if (deletionGame.deletedCount === 0) {
-        throw `Could not delete Game with id of ${id}`;
-      }
-    
-    return name;
-  
-  }
-  
-  
+  return name;
+}
 
 module.exports = {
-    create,
-    update,
-    getAll,
-    getOne,
-    remove
+  create,
+  update,
+  getAll,
+  getOne,
+  remove
 }
