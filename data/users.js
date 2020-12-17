cf010f8ddf09e0dbca07c687f3a17bbbb613245f
Raj3124 <rajmj3113@gmail.com>
2020-12-17 00:45:05 -0500

    Removed unnecessary code and corrected ID check

--- data/users.js
+++ data/users.js
@@ -1,5 +1,5 @@
 const mongoCollections = require('../config/mongoCollections.js');
-let { ObjectId } = require('mongodb');
+let  objectId  = require('mongodb').ObjectId;
 const users = mongoCollections.users;
 
 const create = async (first_name,last_name,username,age,email,admin,state,country,hashedPassword) => {
@@ -102,23 +102,11 @@ const create = async (first_name,last_name,username,age,email,admin,state,countr
 }
 
 const remove = async(id, name) => {
-    if (!id) {
-      throw 'You must provide an id to delete user';
-    }
-    if(typeof(id)!="string" || id.length!=24){
-      throw 'You must only pass in id as string that is 24 charecters long'
-    }
-    if(id.trim()===""){
-      throw 'Id can not be empty spaces'
-    }
-    var re =  /^[0-9a-fA-F]+$/;
-    if(!re.test(id)) {
-      throw 'Given Input is not in hexadecimal please verify ID'
-    } 
+    check_id(id)
     usersCollect = await users();
     const deletionGame = await usersCollect.deleteOne({ _id: ObjectId(id) });
       if (deletionGame.deletedCount === 0) {
-        throw `Could not delete Game with id of ${id}`;
+        throw `Could not delete Game.}`;
       }
     
     return name;
@@ -135,25 +123,16 @@ const check_usernames = async(username) =>{
   }
 
 const check_id = async(userid) =>{
-    if (!userid) {
-        throw 'You must provide an userid to delete user';
-      }
-      if(typeof(userid)!="string" || userid.length!=24){
-        throw 'You must only pass in userid as string that is 24 charecters long'
-      }
-      if(userid.trim()===""){
-        throw 'Id can not be empty spaces'
-      }
-      var re =  /^[0-9a-fA-F]+$/;
-      if(!re.test(userid)) {
-        throw 'Given Input is not in hexadecimal please verify ID'
-      } 
-    const usersCollect = await users();
-    const usernameList = await usersCollect.findOne({"_id": ObjectId(userid)});
-    if(usernameList!=null){
-        return true
+    if (!userid || typeof userid !== 'string') {
+        throw `ID is not proper`
     }
-    return false
+    userid = userid.trim()
+    if (userid == "") {
+        throw `ID should not be blank`
+    }
+    if (objectId.isValid(userid) === false) {
+        throw `ID is not valid`
+    }
   }
 
 const getAllUsers = async () => {
@@ -172,6 +151,5 @@ module.exports = {
     create,
     remove,
     check_usernames,
-    check_id,
     getAllUsers
 }
