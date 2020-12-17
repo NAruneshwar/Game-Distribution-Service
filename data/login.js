704c9e31c845f35224b3eecb07ce9acf564730a1
hbishnoi <hbishnoi@stevens.edu>
2020-12-17 16:33:12 -0500

    updated code

--- data/login.js
+++ data/login.js
@@ -1,34 +1,34 @@
 // for data related to login
-const mongoCollections = require('../config/mongoCollections.js');
+const mongoCollections = require("../config/mongoCollections.js");
 const users = mongoCollections.users;
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
 
 // create a function called check(username,hashedPassword) to check username and password of user
 const check = async (username, hash) => {
-    if (!username || typeof (username) != 'string') {
-        throw 'You must provide a username for the user to login in a string format';
-    }
-    if (username.trim() === "") {
-        throw 'the given username is empty string please provide a username'
-    }
-    if (!hash || typeof (hash) != 'string') {
-        throw 'You must provide a password for the user in a string format';
-    }
-    if (hash.trim() === "") {
-        throw 'the given password is empty string please provide a password for the user'
-    }
+  if (!username || typeof username != "string") {
+    throw "You must provide a username for the user to login in a string format";
+  }
+  if (username.trim() === "") {
+    throw "the given username is empty string please provide a username";
+  }
+  if (!hash || typeof hash != "string") {
+    throw "You must provide a password for the user in a string format";
+  }
+  if (hash.trim() === "") {
+    throw "the given password is empty string please provide a password for the user";
+  }
 
-    usersCollect = await users();
-    const userDetails = await usersCollect.findOne({ "username": username })
-    if (userDetails == null) {
-        throw `Username or password is Incorrect!`
-    }
-    compared = await bcrypt.compare(hash, userDetails.hashedPassword)
-    if (compared == false) {
-        throw `Username or password is Incorrect!`
-    }
-    return userDetails;
-}
+  usersCollect = await users();
+  const userDetails = await usersCollect.findOne({ username: username });
+  if (userDetails == null) {
+    throw `Username or password is Incorrect!`;
+  }
+  compared = await bcrypt.compare(hash, userDetails.hashedPassword);
+  if (compared == false) {
+    throw `Username or password is Incorrect!`;
+  }
+  return userDetails;
+};
 
 // create a function called admin_check(username,hashedPassword) to check username and password of admin
 // const admin_check = async(username,hash) =>{
@@ -58,6 +58,6 @@ const check = async (username, hash) => {
 // }
 
 module.exports = {
-    check
-    // admin_check
-}
+  check,
+  // admin_check
+};
